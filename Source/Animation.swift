a4007c872d82d137613906838186c2c64f26f7bc
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-20 00:30:15 +0200

    Adds the second animation type

--- Source/Animation.swift
+++ Source/Animation.swift
@@ -18,8 +18,19 @@ extension CALayer {
     addAnimation(animation, forKey: nil)
   }
 
-  public func animateBezier<T>(property: Animation.Property, to: T, _firstX: CGFloat, _firstY: CGFloat, _secondX: CGFloat, _secondY: CGFloat, duration: NSTimeInterval) {
+  public func animateBezier(property: Animation.Property, to: NSValue, bezierPoints: [Float], duration: NSTimeInterval) {
+    let animation = BakerAnimation(keyPath: property.rawValue)
+    let initialValue = Animation.propertyValue(property, layer: self)
 
+    animation.values = [initialValue, to]
+    animation.keyTimes = [0, duration]
+    animation.duration = duration
+    animation.removedOnCompletion = false
+    animation.fillMode = kCAFillModeForwards
+    animation.timingFunction = CAMediaTimingFunction(controlPoints:
+      bezierPoints[0], bezierPoints[1], bezierPoints[2], bezierPoints[3])
+
+    addAnimation(animation, forKey: nil)
   }
 
   public func animateSpring<T>(property: Animation.Property, to: T, _tension: CGFloat, _friction: CGFloat, _velocity: CGFloat) {
