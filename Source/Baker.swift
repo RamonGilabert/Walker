649d4ea47e06cb4b28553b145576c6fbb4d0c1bb
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-19 21:20:40 +0200

    Adds another struct

--- Source/Baker.swift
+++ Source/Baker.swift
@@ -5,18 +5,30 @@ struct Baker {
   static var velocity: CGFloat = 0
 
   static func animate(curve: Animation.AnimationCurve, duration: NSTimeInterval, finalValue: CGFloat) {
-    if curve == Animation.AnimationCurve.Linear {
-      velocity = finalValue / CGFloat(duration)
-    } else if curve == Animation.AnimationCurve.EaseIn {
-
-    } else if curve == Animation.AnimationCurve.EaseOut {
-
-    } else if curve == Animation.AnimationCurve.EaseInOut {
-      
-    }
+    let time = Timing.timing(curve, duration)
   }
 
   // MARK: - CubicBezier
 
   // MARK: - Spring
+}
+
+struct Timing {
+
+  static func timing(curve: Animation.AnimationCurve, _ duration: NSTimeInterval) -> NSTimeInterval {
+    switch curve {
+    case .Linear:
+      return duration
+    case .EaseIn:
+      return duration * duration * duration
+    case .EaseOut:
+      return pow(duration - 1, 3) + 1
+    case .EaseInOut:
+      return duration < 0.5 ? pow(duration, 3) * 4 : pow(2 * duration - 2, 2) * (duration - 1) + 1
+    }
+  }
+}
+
+struct Interpolation {
+
 }
