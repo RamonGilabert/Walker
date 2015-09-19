7611baa6eff70250bcd29b3f33f47707083e89cd
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-19 19:27:28 +0200

    Adding the module

--- Demo/Bakery/Bakery/ViewController.swift
+++ Demo/Bakery/Bakery/ViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-import Baker
+import Bakery
 
 class ViewController: UIViewController {
 
---
+++ Demo/Bakery/Pods/Baker/Bakery.h
@@ -0,0 +1,19 @@
+//
+//  Baker.h
+//  Baker
+//
+//  Created by Ramon Gilabert Llop on 9/19/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Baker.
+FOUNDATION_EXPORT double BakerVersionNumber;
+
+//! Project version string for Baker.
+FOUNDATION_EXPORT const unsigned char BakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Baker/PublicHeader.h>
+
+
