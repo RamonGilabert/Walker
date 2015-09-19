e91859cd78687dd5b1e18481918dec0f3799243c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-19 23:16:22 +0200

    Adds another class

--- Source/Animation.swift
+++ Source/Animation.swift
@@ -2,8 +2,11 @@ import Foundation
 
 extension CALayer {
 
-  public func animate<Value>(property: Animation.Property, to: Value, duration: NSTimeInterval, curve: Animation.Curve) {
-    let animation = CAKeyframeAnimation(keyPath: property.rawValue)
+  public func animate<Value>(property: Animation.Property, from: Value, to: Value, duration: NSTimeInterval, curve: Animation.Curve) {
+    let animation = BakerAnimation(keyPath: property.rawValue)
+    animation.fromValue = from as! CGFloat
+    animation.toValue = from as! CGFloat
+    addAnimation(animation, forKey: property.rawValue)
   }
 
   public func animateBezier<T>(property: Animation.Property, to: T, _firstX: CGFloat, _firstY: CGFloat, _secondX: CGFloat, _secondY: CGFloat, duration: NSTimeInterval) {
@@ -12,5 +15,18 @@ extension CALayer {
 
   public func animateSpring<T>(property: Animation.Property, to: T, _tension: CGFloat, _friction: CGFloat, _velocity: CGFloat) {
     
+  }
+}
+
+class BakerAnimation: CAKeyframeAnimation {
+  var fromValue: CGFloat = 0
+  var toValue : CGFloat = 0
+
+  override init() {
+    super.init()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
   }
 }
