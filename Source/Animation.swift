d37709c53d95009672545753f2af2ce5d115f67f
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-09-19 19:36:50 +0200

    Adds different values

--- Source/Animation.swift
+++ Source/Animation.swift
@@ -13,17 +13,15 @@ public struct Animation {
   public var duration: NSTimeInterval!
   public var delay: NSTimeInterval?
 
-  public static func animate(curve: AnimationCurve, duration: NSTimeInterval, animations: (() -> Void)!) {
-    UIView.animateWithDuration(0.4, animations: {
-      animations()
-    })
-  }
-
-  public static func cubicBezierAnimation(firstX: CGFloat, firstY: CGFloat, secondX: CGFloat, secondY: CGFloat, duration: NSTimeInterval, animations: (() -> Void)!) {
+  public static func animate(curve: AnimationCurve, duration: NSTimeInterval, animations: () -> Void) {
 
   }
 
-  public static func springAnimation(tension: CGFloat, friction: CGFloat, velocity: CGFloat, animations: (() -> Void)!) {
+  public static func cubicBezierAnimation(firstX: CGFloat, firstY: CGFloat, secondX: CGFloat, secondY: CGFloat, duration: NSTimeInterval, animations: () -> Void) {
+
+  }
+
+  public static func springAnimation(tension: CGFloat, friction: CGFloat, velocity: CGFloat, animations: () -> Void) {
     
   }
 }
