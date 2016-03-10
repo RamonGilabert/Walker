6a1b175ba42ea0190c63e06076becceb377d6e3f
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-10 11:33:08 +0100

    Adds the spring file

---
+++ Source/Spring.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+public func spring(view: UIView, spring: CGFloat, friction: CGFloat,
+  mass: CGFloat, tolerance: CGFloat = 0.0001, animations: Bake -> Void) -> Bakery {
+
+    return Bakery.bakery
+}
