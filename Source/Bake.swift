7146515c549bb0400e18877c792f43124a4e8164
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2016-03-11 00:40:17 +0100

    Adds the documentation for bake

--- Source/Bake.swift
+++ Source/Bake.swift
@@ -5,83 +5,143 @@ public class Bake: Equatable {
   internal enum Kind {
     case Bezier, Spring
   }
-  
+
+  /**
+   Change the opacity of the layer.
+   */
   public var alpha: CGFloat {
     didSet { alpha(alpha) }
   }
 
+  /**
+   Change the x position in the anchor point (0, 0).
+   */
   public var x: CGFloat {
     didSet { x(x) }
   }
 
+  /**
+   Change the y position in the anchor point (0, 0).
+   */
   public var y: CGFloat {
     didSet { y(y) }
   }
 
+  /**
+   Change the width of the layer.
+   */
   public var width: CGFloat {
     didSet { width(width) }
   }
 
+  /**
+   Change the height of the layer.
+   */
   public var height: CGFloat {
     didSet { height(height) }
   }
 
+  /**
+   Change the origin of the view in the anchor point (0, 0)
+   */
   public var origin: CGPoint {
     didSet { origin(origin.x, origin.y) }
   }
 
+  /**
+   Change the size of the view.
+   */
   public var size: CGSize {
     didSet { size(size.width, size.height) }
   }
 
+  /**
+   Change the frame of the view.
+   */
   public var frame: CGRect {
     didSet { frame(frame.origin.x, frame.origin.y, frame.width, frame.height) }
   }
 
+  /**
+   Change the cornerRadius of the layer.
+   */
   public var radius: CGFloat {
     didSet { radius(radius) }
   }
 
+  /**
+   Apply a transform value, can be any `CGAffineTransform`.
+   */
   public var transform: CGAffineTransform {
     didSet { transform(transform) }
   }
 
+  /**
+   Change the opacity of the layer.
+   */
   public func alpha(value: CGFloat) {
     animate(.Opacity, value)
   }
 
+  /**
+   Change the x position in the anchor point (0, 0).
+   */
   public func x(value: CGFloat) {
     animate(.PositionX, value + view.frame.width / 2)
   }
 
+  /**
+   Change the y position in the anchor point (0, 0).
+   */
   public func y(value: CGFloat) {
     animate(.PositionY, value + view.frame.height / 2)
   }
 
+  /**
+   Change the width of the layer.
+   */
   public func width(value: CGFloat) {
     animate(.Width, value)
   }
 
+  /**
+   Change the height of the layer.
+   */
   public func height(value: CGFloat) {
     animate(.Height, value)
   }
 
+  /**
+   Change the origin of the view in the anchor point (0, 0)
+   */
   public func origin(x: CGFloat, _ y: CGFloat) {
     animate(.Origin, NSValue(CGPoint: CGPoint(x: x + view.frame.width / 2, y: y + view.frame.height / 2)))
   }
 
+  /**
+   Change the size of the view.
+   */
   public func size(width: CGFloat, _ height: CGFloat) {
     animate(.Size, NSValue(CGSize: CGSize(width: width, height: height)))
   }
 
+  /**
+   Change the frame of the view.
+   */
   public func frame(x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) {
     animate(.Frame, NSValue(CGRect: CGRect(x: x, y: y, width: width, height: height)))
   }
 
+  /**
+   Change the cornerRadius of the layer.
+   */
   public func radius(value: CGFloat) {
     animate(.CornerRadius, value)
   }
 
+  /**
+   Apply a transform value, can be any `CGAffineTransform`.
+   */
   public func transform(value: CGAffineTransform) {
     animate(.Transform, NSValue(CATransform3D: CATransform3DMakeAffineTransform(value)))
   }
